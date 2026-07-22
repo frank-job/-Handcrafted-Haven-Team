6ec711edf2487f0ebb1841a7397b1685b935860c
Boiketlo20 <nzimandeyolanda058@gmail.com>
2026-07-22 06:50:05 +0200

    Set up initial UI for seller dashboard

--- app/ui/Nav/NavLinks.tsx
+++ app/ui/Nav/NavLinks.tsx
@@ -1,1 +1,51 @@
 // NavLinks
+// Navigation links expected in the sellers dashboard - Boiketlo
+
+'use client'
+
+import {
+  CubeIcon,
+  HomeIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard/seller', icon: HomeIcon },
+  {
+    name: 'Profile',
+    href: '/dashboard/profile',
+    icon: UserIcon,
+  },
+  { name: 'Products', href: '/dashboard/products', icon: CubeIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-black hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'hover:bg-black hover:text-white': pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
---
+++ app/ui/Nav/sidenav.tsx
@@ -0,0 +1,23 @@
+// Side navigation for sellers dashboard - Boiketlo
+
+import Link from 'next/link';
+import NavLinks from './NavLinks';
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+      </div>
+    </div>
+  );
+}
+
