cec3aa3db901b7c16bc07640b8a348e62a218c39
frank-job <jobf0837@gmail.com>
2026-07-23 13:12:07 +0300

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="w-full text-black py-4 flex justify-between items-center mb-16">
             <div className="flex gap-2">
-                <span>Haven Handicraft</span>
+               Handcraft <span className='text-blue-500'>  Haven</span>
             </div>           
         </header>
     )
